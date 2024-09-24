1d95cae2f41768ba308ba012721e7d4e8e2c1aa3
Oussamahamani <oussamahamani2000@gmail.com>
2024-09-24 11:36:05 -0400

    ustState review

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,24 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Posts from "./pages/homePage/Posts"
 function App() {
+  const [count,setCount]=useState(0)
 
+  let num = 10
+  
 
+  const increment= ()=>{
+    num++
+    console.log(count,"before")
+    let total = count+1
+    setCount(total)
+    console.log(total,"after")
+    return <p>{num}</p>
+  }
   return (
     <>
-   <Posts/>
+   {/* <Posts/> */}
+   {count}
+   <button onClick={increment}>increment</button>
     </>
   )
 }
